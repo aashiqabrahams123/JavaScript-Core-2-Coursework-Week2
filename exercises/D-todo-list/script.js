bc67648edea649e161b73517276efd9f1b59e62f
Aashiq Abrahams <aashiqabrahams44@gmail.com>
2021-03-30 21:34:57 +0200

    d-todo-list-completed

--- exercises/D-todo-list/script.js
+++ exercises/D-todo-list/script.js
@@ -1,5 +1,22 @@
 function todoList(todos) {
   // Write your code here...
+  
+  let content = document.getElementById('content');
+let ul = document.createElement('ul');
+
+todos.forEach(val => {
+  let li = document.createElement('li');
+  li.innerHTML = val.todo;
+  li.addEventListener('click', e => {
+    if(e.target.style.textDecoration === 'line-through') {
+      e.target.style.textDecoration = 'none'
+    } else {
+      e.target.style.textDecoration = 'line-through';
+    }
+  })
+  ul.append(li);
+})
+  content.append(ul); 
 }
 
 const todos = [
