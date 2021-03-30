59b50ad0c609606d46b242c3edbe5eef5d8eb2b1
Aashiq Abrahams <aashiqabrahams44@gmail.com>
2021-03-30 21:43:09 +0200

    e-list-of-colours-completed

--- exercises/E-list-of-colours/script.js
+++ exercises/E-list-of-colours/script.js
@@ -1,5 +1,24 @@
 function listOfColours(colours) {
   // Write your code here...
+  let content = document.getElementById("content");
+  let select = document.createElement("select");
+  let pGraph = document.createElement("p");
+
+  content.appendChild(select);
+  content.appendChild(pGraph);
+
+  colours.forEach((colour) => {
+    let option = document.createElement("option");
+    option.innerText = colour;
+    option.value = colour;
+    select.appendChild(option);
+  });
+
+  pGraph.innerText = `You have selected: `;
+  select.addEventListener("click", function () {
+    pGraph.innerText = `You have selected: ${select.value}`;
+    pGraph.style.color = select.value;
+  });
 }
 
 const colours = ["red", "blue", "green", "yellow", "pink", "brown"];
