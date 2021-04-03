3694e279e2a313f52353bc441961272d91c38e1a
Aashiq Abrahams <aashiqabrahams44@gmail.com>
2021-04-03 20:38:10 +0200

    blue button function added

---
+++ projects/1-bike-project/js/main.js
@@ -0,0 +1,8 @@
+const blueBtn = document.getElementById("blueBtn");
+blueBtn.addEventListener("click", function() {
+    document.getElementsByClassName("jumbotron")[0].getElementsByClassName.backgroundColor = "#588fbd";
+    document.getElementsByClassName("btn btn-primary btn-lrg")[0].getElementsByClassName.backgroundColor = "#ffa500";
+    document.getElementsByClassName("btn btn-secondary btn-lrg")[0].style.backgroundColor = "black";
+    document.getElementsByClassName("btn  btn-secondary btn-lrg")[0].style.color = "white";
+});
+
