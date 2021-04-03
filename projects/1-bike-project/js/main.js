d40fa1be03ac42ac21dcf65eb7980162d738a798
Aashiq Abrahams <aashiqabrahams44@gmail.com>
2021-04-03 21:20:10 +0200

    green button function added

--- projects/1-bike-project/js/main.js
+++ projects/1-bike-project/js/main.js
@@ -15,3 +15,11 @@ orangeButton.addEventListener("click", function() {
     document.getElementsByClassName("btn btn-secondary btn-lrg")[0].style.backgroundColor = "#31b0d5";
     document.getElementsByClassName("btn btn-secondary btn-lrg")[0].style.color = "white";
 });
+
+const greenButton = document.getElementById("greenBtn");
+greenButton.addEventListener("click", function() {
+    document.getElementsByClassName("jumbotron")[0].style.backgroundColor = "#87ca8a";
+    document.getElementsByClassName("btn btn-primary btn-lrg")[0].style.backgroundColor = "black";
+    document.getElementsByClassName("btn btn-secondary btn-lrg")[0].style.backgroundColor = "#8c9c08";
+    document.getElementsByClassName("btn btn-secondary btn-lrg")[0].style.color = "black";
+});
