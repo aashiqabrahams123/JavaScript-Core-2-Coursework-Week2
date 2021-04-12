e677b5b532bd0006c154e7183fcc0ea63e71bf73
Aashiq Abrahams <aashiqabrahams44@gmail.com>
2021-04-12 19:32:31 +0200

    project-todo-list-element created

--- projects/2-todo-list/script.js
+++ projects/2-todo-list/script.js
@@ -1,7 +1,45 @@
 function populateTodoList(todos) {
   let list = document.getElementById("todo-list");
   // Write your code to create todo list elements with completed and delete buttons here, all todos should display inside the "todo-list" element.
+  todos.forEach((element) => {
+  let liElement = document.createElement("li");
+  liElement.className = "list-group-item d-flex justify-content-between align-items-center";
+  liElement.innerHTML = element.task;
+  console.log(liElement);
+
+  let spanElement = document.createElement("span");
+  spanElement.className = "badge bg-primary rounded-pill";
+
+  let iCheckElement = document.createElement("i");
+  iCheckElement.className = "fa fa-check";
+  iCheckElement.addEventListener("click", () => {
+    if (liElement.style.textDecoration === "none") {
+      liElement.style.textDecoration = "line-through";
+    } else {
+    liElement.style.textDecoration = "none";
+    }
+  });
+
+  let iDeleteElement = document.createElement("i");
+  iDeleteElement.className = "fa fa-delete";
+  iDeleteElement.addEventListener("click", () => {
+    liElement.remove
+  });
+
+  console.log(iDeleteElement);
+  console.log(iCheckElement);
+  console.log(spanElement);
+  liElement.appendChild(iCheckElement);
+  spanElement.appendChild(iDeleteElement);
+  list.appendChild(liElement);
+});
 }
+
+
+
+
+    
+  
 
 // These are the same todos that currently display in the HTML
 // You will want to remove the ones in the current HTML after you have created them using JavaScript
